d7cc7124449d2f019f97ec75bbfe9813c0bddf8d
lamakye7 <75229547+lamakye7@users.noreply.github.com>
2025-07-03 06:37:51 +0100

    Update data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -26,15 +26,13 @@ def process_data(all_cars):
     df["Year"] = df["Car Name"].str.extract(r"(\d{4})").astype(float).astype("Int64")
 
     # Convert Price to Integer
-    df["Price"] = pd.to_numeric(
-        df["Price"].str.replace("₦", "").str.replace(",", ""), errors="coerce"
-    ).astype("Int64")
+    # df["Price"] = pd.to_numeric(
+    #     df["Price"].str.replace("₦", "").str.replace(",", ""), errors="coerce"
+    # ).astype("Int64")
 
     # Convert Mileage to miles
     df["Mileage"] = df["Mileage"].apply(convert_mileage)
 
-    # Reorder Columns
-    df = df[["Year", "Car Name", "Condition", "Mileage", "Engine Type", "Price", "Location"]]
 
     # Save to CSV
     df.to_csv('autochek_cars.csv', index=False)
